078edd4ee30cd4074841a2c4ef732a56e5f96399
abuudarda <abu.darda@g.bracu.ac.bd>
2023-10-12 11:24:03 +0600

    tester

--- t.py
+++ t.py
@@ -1,27 +1,14 @@
-# Sample dictionary 'labels'
-labels = {
-    'item1': 'A',
-    'item2': 'B',
-    'item3': 'A',
-    'item4': 'C',
-    'item5': 'B',
-}
+my_list = [(3, 1), (1, 2), (4, 3), (2, 4)]
 
-# Initialize an empty dictionary to store sets of keys with the same value
-value_sets = {}
 
-# Iterate through the items in the 'labels' dictionary
-for key, value in labels.items():
-    # Check if the value is already a key in the 'value_sets' dictionary
-    if value in value_sets:
-        # If it is, add the key to the existing set
-        value_sets[value].add(key)
-    else:
-        # If it's not, create a new set with the key and store it in the 'value_sets' dictionary
-        value_sets[value] = {key}
+# Sort each tuple within the list
+# sorted_list = [tuple(sorted(t)) for t in my_list]
+# # Sort the list of tuples by both elements of each tuple
+# sorted_list = sorted(sorted_list, key=lambda x: (x[0], x[1]))
+# for index,edge in enumerate(my_list):
+#     if(edge[0]>edge[1]):
+#         my_list[index]=(edge[1],edge[0])
+# print(my_list)
 
-# Convert the dictionary of sets to a list of sets
-list_of_sets = list(value_sets.values())
-
-# Print the result
-print(list_of_sets)
+my_list=[tuple(sorted(t)) for t in my_list]
+print(my_list)
